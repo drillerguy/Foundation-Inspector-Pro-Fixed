e883d0ee5a9a454a1f853b9d4d7badcb0f1a6c0e
drillerguy <mikew@s-ug.net>
2026-08-06 17:41:11 -0500

    chore: refresh reliable sharing cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='fieldverify-pro-v25';
+const CACHE='fieldverify-pro-v26';
 const CORE=['./','./index.html','./caisson-plan.png','./caisson-data.js','./xlsx.full.min.js','./pdf.min.mjs','./pdf.worker.min.mjs','./pdf-lib.min.js','./manifest.webmanifest','./recovery.html'];
 
 self.addEventListener('install',event=>{
