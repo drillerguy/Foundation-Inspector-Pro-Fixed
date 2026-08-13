39c1df76f6090dc7414ceb8144d5cf4373510e50
drillerguy <mikew@s-ug.net>
2026-08-13 15:55:33 -0500

    Add Dropbox cloud sharing shell

---
+++ dropbox-cloud-v104.js
@@ -0,0 +1,7 @@
+/* FieldVerify Pro Dropbox cloud office sharing v10.4.0 */
+(() => {
+  'use strict';
+  const VERSION='10.4.0';
+  window.FIELDVERIFY_DROPBOX={version:VERSION,connected:false};
+  console.info(`FieldVerify Dropbox cloud sharing v${VERSION} loaded`);
+})();
