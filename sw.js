afcaf7c15c93cded1c14b3eed9de836e952746ab
drillerguy <mikew@s-ug.net>
2026-08-31 17:22:25 -0500

    Point canonical shim to stable v10.25.42 worker

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 /* Canonical FieldVerify Pro worker shim. Keep this file changed with each release so iOS checks the current worker exactly once. */
-importScripts('./service-worker.js?v=10.25.41');
+importScripts('./service-worker.js?v=10.25.42');
