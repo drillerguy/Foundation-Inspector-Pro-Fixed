5ad62a982ff9e1fb24c7234703138ce3e10f0307
drillerguy <mikew@s-ug.net>
2026-09-01 08:42:59 -0500

    Point legacy worker shim to v10.25.48

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Legacy compatibility shim for FieldVerify Pro v10.25.47. */
-importScripts('./service-worker.js?v=10.25.47');
+/* Legacy compatibility shim for FieldVerify Pro v10.25.48. */
+importScripts('./service-worker.js?v=10.25.48');
